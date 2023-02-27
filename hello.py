37b76b3b65bdf344a7e92525a0e4503834a8b35e
huizhuansam <huizhuansam@hotmail.com>
2023-02-27 17:59:32 +0800

    update configs

--- hello.py
+++ hello.py
@@ -2,5 +2,14 @@ from prefect import flow
 
 
 @flow
+def setup():
+    print("Setting up...")
+
+@flow
 def hello():
+    setup()
     print(f"Hello, Prefect! 🙌")
+
+if __name__ == "__main__":
+    hello()
+    
